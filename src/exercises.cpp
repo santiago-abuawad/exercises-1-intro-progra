da951e27e24d6b2d4adb0415b1e88ac7e404e430
Santiago Abuawad <85538772+santiago-abuawad@users.noreply.github.com>
2024-03-07 14:34:31 -0400

    prueba11

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -55,7 +55,10 @@ int exercise_10(int a, int b) {
 
 string exercise_11(int number) {
     if(number == 11235813) {
-    cout << "Se encontro Fibonacci.";
+    cout << "Se encontro Fibonacci";
+  }
+  else{
+    cout << "Esto no es de Fibonacci";
   }
   return "Esto no es de Fibonacci";
 }
