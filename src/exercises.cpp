e4028e2328dc3baad9e5c5dfe72132577cea409f
Santiago Abuawad <85538772+santiago-abuawad@users.noreply.github.com>
2024-03-08 17:30:33 -0400

    prueba1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -33,7 +33,12 @@ void exercise_2(double A, double B, double C) {
 } 
 
 void exercise_3(int a, int b) {
-  // TODO: YOUR CODE HERE
+  if(b == 0) {
+    cout << "Imposible";
+  } else {
+    int c = A / B;
+    cout<<c;
+  }
 }
 
 void exercise_4(double n, double a, double b, double x, double y) {
