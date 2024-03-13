67eeafc1656779b89091055cdbc755ccd9c26753
Santiago Abuawad <85538772+santiago-abuawad@users.noreply.github.com>
2024-03-13 01:01:35 -0400

    prueba

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -103,7 +103,7 @@ void exercise_7(double r) {
   if(r > 0 || r == 0) {
     double pi = 3.141592653589793238462643;
     double area = 4*pi*r*2;
-    cout <<area;
+    cout <<area<<"\n";
   } else {
     cout << "Error: Radius cannot be negative.\n";
   }
